59d9913972fd54c4fc9fa6f85aeea1efaadd7b00
Varenie <sanyacorortkih@yandex.ru>
2021-01-03 18:04:41 +0300

    raw acc screen done

---
+++ app/src/main/java/com/varenie/carservice/Adapters/UserClassAccAdapter.kt
@@ -0,0 +1,40 @@
+package com.varenie.carservice.Adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.varenie.carservice.R
+
+//адаптер для спикска машин в профиле
+class UserClassAccAdapter: RecyclerView.Adapter<UserClassAccAdapter.MyHolder>() {
+
+    class MyHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val brend = itemView.findViewById<TextView>(R.id.tv_car_brend)
+        val statenumber = itemView.findViewById<TextView>(R.id.tv_car_number)
+        val isService = itemView.findViewById<TextView>(R.id.tv_is_in_service)
+
+        fun bind() {
+//            Тут будет размещаться инфа на элементах
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.acc_cars_recycler_item, parent, false)
+
+        return MyHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        holder.bind()
+    }
+
+    //временное решение, для проверки работоспособности
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+}
--- app/src/main/java/com/varenie/carservice/Fragments/AccountFragment.kt
+++ app/src/main/java/com/varenie/carservice/Fragments/AccountFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.varenie.carservice.Adapters.FavoriteServicesAdapter
+import com.varenie.carservice.Adapters.UserClassAccAdapter
 import com.varenie.carservice.R
 
 class AccountFragment : Fragment() {
@@ -13,6 +17,21 @@ class AccountFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        val root = inflater.inflate(R.layout.fragment_account, container, false)
+
+        val carRecycler = root.findViewById<RecyclerView>(R.id.rv_acc_cars_recycler)
+        val servicesRecycler = root.findViewById<RecyclerView>(R.id.rv_favorite_services)
+        val carAdapter = UserClassAccAdapter()
+        val servicesAdapter = FavoriteServicesAdapter()
+
+        carRecycler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false) // для горизонтального списка
+        carRecycler.setHasFixedSize(true)
+        servicesRecycler.layoutManager = LinearLayoutManager(requireContext())
+        servicesRecycler.setHasFixedSize(true)
+
+        carRecycler.adapter = carAdapter
+        servicesRecycler.adapter = servicesAdapter
+
+        return root
     }
 }
